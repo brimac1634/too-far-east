0e0fb1bfe7f8411fee1fb92928b1fe144bcc8451
Brian MacPherson <brimac1634@gmail.com>
2019-09-02 12:27:55 +0800

    added more to home section and our story

--- src/components/home/home.component.js
+++ src/components/home/home.component.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
+import CustomButton from '../custom-button/custom-button.component';
 import MenuButton from '../menu-button/menu-button.component';
+import { ReactComponent as Logo } from '../../assets/logo.svg'
 
 import './home.styles.scss';
 
@@ -10,6 +12,19 @@ const Home = () => (
 			<div className='menu-button'>
 				<MenuButton inverted />
 			</div>
+		</div>
+		<div className='center'>
+			<div className='logo-container'>
+				<Logo />
+			</div>
+			<CustomButton>
+				Book Now
+			</CustomButton>
+		</div>
+		<div className='scroll-message'>
+			<span className='bob'>&#8595;</span>
+			<span>scroll down for more</span>
+			<span className='bob'>&#8595;</span>
 		</div>
 	</div>
 )
--- src/components/our-story/our-story.component.js
+++ src/components/our-story/our-story.component.js
@@ -4,7 +4,11 @@ import './our-story.styles.scss';
 
 const OurStory = () => (
 	<div className='our-story'>
-		Hi
+		<div className='text'>
+			<h1>OUR STORY</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+		</div>
 	</div>
 )
 
