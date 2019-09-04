6dcad531b0730dbbb794f5d6730999291862fa17
Brian MacPherson <brimac1634@gmail.com>
2019-09-04 21:54:24 +0800

    started to adjust gallery

--- src/components/gallery/gallery.component.js
+++ src/components/gallery/gallery.component.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
+import GalleryItem from '../gallery-item/gallery-item.component';
+
 import shanghai from '../../assets/shanghai.jpg';
 import barber from '../../assets/barber.jpg';
 import './gallery.styles.scss';
 
 const Gallery = () => (
 	<div className='gallery'>
-		<img className='image' src={shanghai} alt='shanghai' />
-		<img className='image' src={barber} alt='barber' />
+		<GalleryItem image={shanghai} />
+		<GalleryItem image={barber} />
 	</div>
 )
 
