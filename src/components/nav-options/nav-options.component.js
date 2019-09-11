1e5263e634bd17979d9389e80bfd3f71e934f964
Brian MacPherson <brimac1634@gmail.com>
2019-09-11 15:55:17 +0800

    added admin options to drop menu when user is logged in

--- src/components/nav-options/nav-options.component.js
+++ src/components/nav-options/nav-options.component.js
@@ -1,28 +1,61 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import MediaQuery from 'react-responsive';
 
 import { scrollToSection } from '../../redux/nav/nav.actions';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
+import { signOutStart } from '../../redux/user/user.actions';
+import { toggleMenu } from '../../redux/menu/menu.actions';
 
 import navOptions from './nav-options.data';
 
 import './nav-options.styles.scss'
 
-const mapDisptachToProps = dispatch => ({
-	scrollToSection: section => dispatch(scrollToSection(section))
+const mapStateToProps = state => ({
+	currentUser: selectCurrentUser(state)
 })
 
-const NavOptions = ({ inverted, show, vertical, scrollToSection }) => {
+const mapDisptachToProps = dispatch => ({
+	scrollToSection: section => dispatch(scrollToSection(section)),
+	signOutStart: () => dispatch(signOutStart()),
+	toggleMenu: showMenu => dispatch(toggleMenu(showMenu))
+})
+
+const NavOptions = ({ toggleMenu, currentUser, history, inverted, show, vertical, scrollToSection, signOutStart }) => {
 
 	const handleScroll = section => {
 		scrollToSection(section)
+		toggleMenu(false)
 		setTimeout(()=>scrollToSection(null), 1000)
+	}
+
+	const handleNewUpdate = () => {
+		history.push('/new-update')
+		toggleMenu(false)
 	}
 
 	return (
 		<MediaQuery maxWidth={1024}>
 			{(matches) =>
 				<div className={`options ${vertical ? 'vertical' : null}`}>
+					{
+						currentUser &&
+						<div className='admin'>
+							<span 
+								className={`option ${inverted ? 'inverted' : null} ${show ? 'show' : 'hide'}`} 
+								onClick={handleNewUpdate}
+							>
+								New Update
+							</span>
+							<span 
+								className={`option ${inverted ? 'inverted' : null} ${show ? 'show' : 'hide'}`} 
+								onClick={signOutStart}
+							>
+								Logout
+							</span>
+						</div>
+					}
 					{
 						navOptions.filter(({title})=>{
 							if (matches && title === 'gallery') {
@@ -47,4 +80,4 @@ const NavOptions = ({ inverted, show, vertical, scrollToSection }) => {
 	)
 }
 
-export default connect(null, mapDisptachToProps)(NavOptions);
+export default withRouter(connect(mapStateToProps, mapDisptachToProps)(NavOptions));
