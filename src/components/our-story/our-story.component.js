8dab6578d33bc55bdbe36cdb0d12c709c72bc935
Brian MacPherson <brimac1634@gmail.com>
2019-10-29 21:58:45 +0800

    updating about us section

--- src/components/our-story/our-story.component.js
+++ src/components/our-story/our-story.component.js
@@ -23,7 +23,7 @@ const OurStory = () => (
 				<h1>OUR STORY</h1>
 			</Fade>
 			<Fade bottom>
-				<p>Too Far East Barber & Co. was founded by two individuals who barbered side by side from the day they entered the industry. One with deep roots in Hong Kong and the other hailing from Toronto, Canada, both have immense respect for the Cantonese and Shanghainese barbers that were here before them. Naturally, Too Far East became a barbershop where East meets with West and where the old is weaved with the new. It represents their journey of going so Far East that they came out West.
+				<p>Too Far East Barber & Co. was founded by two individuals who barbered side by side from the day they entered the industry. One with deep roots in Hong Kong and the other hailing from Canada; both have immense respect for the Cantonese and Shanghainese barbers that were here before them. Naturally, Too Far East became a barbershop where East meets with West and where the old is weaved with the new. It represents their journey of going so far East that they came out West.
 				</p>
 			</Fade>
 		</div>
