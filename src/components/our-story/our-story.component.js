0e4408eb8a19b14c35977d03aa80fc8e36cdd139
Brian MacPherson <brimac1634@gmail.com>
2023-05-22 20:38:05 +0800

    Updated about us text

--- src/components/our-story/our-story.component.js
+++ src/components/our-story/our-story.component.js
@@ -24,7 +24,7 @@ const OurStory = () => (
 			</Enter>
 			<Enter enterStyle='fade-up' duration='1000ms'>
 				<p>
-					Too Far East Barber & Co. draws inspiration from the saying "Too Far East is West," which conveys the notion that if we travel far far away, life will inevitably lead us back to where we started. This philosophy reminds us to respect the old and embrace the new; compelling us to pay tribute to a timeless era of Hong Kong barbershops, expressed in a modern way.  
+					Too Far East Barber & Co. draws inspiration from the saying "Too Far East is West," which conveys the notion that no matter how far we travel, life will inevitably lead us back to where we started. This philosophy reminds us to respect the old and embrace the new; compelling us to pay tribute to a timeless era of Hong Kong barbershops, expressed in a modern way.  
 					<br/><br/>
 					At Too Far East Barber & Co., customers are always welcomed with friendly vibes and complimentary drinks. Expect a unique grooming experience that blends classic and contemporary elements, while perfectly capturing the essence of our boundless journey.
 				</p>
